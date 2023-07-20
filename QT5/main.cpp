fbb3470030b2202ffb26b180e52dbc7e8da520aa
scx005548 <139521579+scx005548@users.noreply.github.com>
2023-07-20 10:05:10 +0000

    Update main.cpp

--- QT5/main.cpp
+++ QT5/main.cpp
@@ -1,11 +1,11 @@
 #include "widget.h"
 #include <QApplication>
-
+#include <QDbug>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Widget w;
     w.show();
-
+    qInfo()<<1;
     return a.exec();
 }
